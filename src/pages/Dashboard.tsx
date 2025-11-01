6aaf35cde238b5b76f3c4e9eb1a659eb55a1afe2
Jhon Williams <prokhamexe@gmail.com>
2025-11-01 17:11:44 +0630

    logout test

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,9 @@
+import { logOut } from "@/lib/auth";
 export const Dashboard = () => {
-  return <>Dashboard</>;
+  return (
+    <>
+      Dashboard
+      <button onClick={logOut}>Logout</button>
+    </>
+  );
 };
