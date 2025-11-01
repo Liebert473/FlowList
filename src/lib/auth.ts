ad1686ed3e0ca8ec48bb13d7d684d7bda74f6f3d
Jhon Williams <prokhamexe@gmail.com>
2025-11-01 16:31:25 +0630

    Added OAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -21,7 +21,7 @@ export const signInWithGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin + "/",
+        redirectTo: `${window.location.origin}/auth/callback`
       },
     });
     if (error) throw error;
