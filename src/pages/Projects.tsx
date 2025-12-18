5cc0eb3207b8a47a562b90014e88e9ce00fe38bc
Jhon Williams <prokhamexe@gmail.com>
2025-12-18 21:49:44 +0630

    update

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,17 @@
-export const Projects = () => {
+import { NotFound } from "@/components/common/NotFound";
+import { TablePlace } from "@/components/table/TablePlace";
+import { useTables } from "@/features/tables/useTables";
 
-  return <>Projects</>;
+export const Projects = () => {
+  const { data: tables, isLoading } = useTables();
+  const currentTable = tables?.filter((t) => t.title == "Projects")[0];
+  return (
+    <>
+      {currentTable || isLoading ? (
+        <TablePlace table={currentTable} />
+      ) : (
+        <NotFound message="Table Not Found." />
+      )}
+    </>
+  );
 };
